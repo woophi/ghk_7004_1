5d283f5615504182d54c0071f3a519f33eadc3fa
Konstantin Mikheev <mik.kon6610@gmail.com>
2026-03-16 12:14:20 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -80,7 +80,6 @@ export const App = () => {
     if (currentQuestion && view === 'quiz') {
       window.gtag('event', '7307_question_show', {
         question_id: `q${currentQuestion.id}`,
-        question_number: `${currentQuestion.id}`,
       });
     }
   }, [currentQuestion, view]);
@@ -89,14 +88,14 @@ export const App = () => {
     if (view === 'quiz') {
       document.body.style.background = '#FFFFFF';
     } else {
-      window.gtag('event', '7307_quiz_show', { var: 'var1' });
+      window.gtag('event', '7307_quiz_show');
       document.body.style.background = 'linear-gradient(172.56deg, #3B0A72 -5.43%, #FDF8BD 40.77%, #08146C 86.97%)';
     }
   }, [view]);
 
   const submit = () => {
     if (finished) {
-      window.gtag('event', '7307_result_show', { var: 'var1' });
+      window.gtag('event', '7307_result_request');
       const fisrtAnswerIndex = questions[0].options.indexOf(
         answers.find(a => a.questionId === questions[0].id)?.answer || '',
       );
@@ -107,7 +106,10 @@ export const App = () => {
       if (!answers.some(a => a.questionId === currentQuestion.id)) {
         return;
       }
-      window.gtag('event', '7307_continue_click', { var: 'var1' });
+      window.gtag('event', '7307_continue_click', {
+        question_id: `q${currentQuestion.id}`,
+        answer_id: `${currentQuestion.options.indexOf(answers.find(a => a.questionId === currentQuestion.id)?.answer || '') + 1}`,
+      });
       setActiveIndex(index => index + 1);
     }
     // sendDataToGA({
@@ -144,12 +146,6 @@ export const App = () => {
             <PureCell
               key={index}
               onClick={() => {
-                window.gtag('event', '7307_answer_select', {
-                  question_id: `q${currentQuestion.id}`,
-                  question_number: `${currentQuestion.id}`,
-                  answer_id: `${index + 1}`,
-                  answer_number: `${index + 1}`,
-                });
                 setAnswers(
                   answers.some(a => a.questionId === currentQuestion.id)
                     ? answers.map(a =>
@@ -218,7 +214,7 @@ export const App = () => {
           block
           view="secondary"
           onClick={() => {
-            window.gtag('event', '7307_quiz_start', { var: 'var1' });
+            window.gtag('event', '7307_quiz_start');
             setView('quiz');
           }}
           className={appSt.btnWhite}
